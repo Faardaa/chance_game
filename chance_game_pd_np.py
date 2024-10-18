944a62c13278c65c7728ea95cab9e0749322e888
Faardaa <136010128+Faardaa@users.noreply.github.com>
2024-10-19 00:50:10 +0400

    chance_game_pd_np.py

--- chance_game_pd_np.py
+++ chance_game_pd_np.py
@@ -5,11 +5,11 @@ class GameOfLuck:
     def __init__(self, initial_amount):
         self.amount = initial_amount
 
-    # The main part of the game
+    # Generating game requirements
     def game_inside(self):
         rows = range(1, 11)
         cols = range(1, 11)
-        data = np.random.choice(['Win', 'Lose'], size=(10, 10)) # Creates a 10x10 list which contains 100 "Lose" or "Win" messages.
+        data = np.random.choice(['x3', 'x2', '=1', '/2', '/3', '/4'], size=(10, 10)) # Generates a 10x10 array filled with random selections from the specified list of outcomes.
         random_df = pd.DataFrame(data, index = rows, columns = cols)
 
         while True:
@@ -18,51 +18,47 @@ class GameOfLuck:
                 break
             print("You have to enter a number between 1 to 10 !!!")
         
-        if self.win_or_not(random_df, int(row) - 1, int(col) - 1) : # Defines if user won or not 
-            self.amount *= 2
-            self.play_again()
-            return
-        else:
-            # User looses the game so, reduce the amount
-            self.amount /= 2
-            self.play_again()
-            return
+        self.amount_counter(random_df, int(row) - 1, int(col) - 1)
             
     # Defines if user is won to the game or not
-    def win_or_not(self, df, row, col):
-        print(f"From this unique table: \n{df}")
-        print(f"You {df.iloc[row, col]} 2x of the price... \nChoices ({row + 1}, {col + 1}) ")
+    def amount_counter(self, df, row, col):
+        print(f"\nFrom this unique table: \n{df}")
+        print(f"Your choices: ({row + 1}, {col + 1})\nYou got {df.iloc[row, col]} of your money...")
 
         # If user wins the game function returns True otherwise False
-        if df.iloc[row, col] == "Win":
-            return True
-        else:
-            return False
+        gain = df.iloc[row, col]
+        if gain[0] == "x":
+            self.amount *= int(gain[1])
+        elif gain[0] == "/":
+            self.amount /= int(gain[1])
         
-    def play_again(self):
-        print('Current amount:', self.amount)
-        if self.amount < 1:
-            print("Current amount is less than 1$ so Game is Over!!! : ")
-            return
-
-        # Asks user of he/she will play again
+    # Root function for the game.
+    def play_game(self):
+        # Main loop of the game.
         while True:
-            play_again = input(f"Play again for {self.amount * 2}$? (yes/no): ").lower()
-            if play_again == 'yes':
-                GameOfLuck.main(self.amount)
-            elif play_again == 'no':
-                print("Your money preparing, see you soon :)")
-                break
-            else:
-                print('Only "yes" and "no" accepted.')
+            self.game_inside() 
+            print('Current amount:', self.amount)
+            if self.amount < 1:
+                print("Current amount is less than 1$ so Game is Over!!!")
+                return
+
+            # Asks user of he/she will play again
+            while True:
+                play_again = input(f"Play again? (yes/no): ").lower()
+                if play_again == 'yes':
+                    break
+                elif play_again == 'no':
+                    print("Your money preparing, see you soon :)")
+                    return
+                else:
+                    print('Only "yes" and "no" accepted.')
 
     @staticmethod
-    def main(initial_amount = 0):
-        if not initial_amount:
-            print("***** Welcome to Game of Luck *****")
-            initial_amount = int(input('Enter initial amount (No coins):')) # Provided by cash acceptor
+    def main():
+        print("~~~~~~~~~~~~~~~~ Welcome to Game of Luck ~~~~~~~~~~~~~~~~")
+        initial_amount = int(input('Enter initial amount (No coins):')) # Provided by cash acceptor tool (no need to error handling)
         game = GameOfLuck(initial_amount)
-        game.game_inside()
+        game.play_game()
         
 
 if __name__ == '__main__':
